ca55ede3c502d793d11ca80484d5a21d0f068d85
andreas ubertini <ubertiniandreas@gmail.com>
2023-06-27 17:42:44 +0200

    javascprit 60%

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 const hideSectionButtons = document.getElementsByClassName('nascondi')
 const toHide1 = document.getElementById('saldi')
 const toHide2 = document.getElementById('welcome-summer')
-hideSectionButtons[0].addEventListener('click', function (){
+hideSectionButtons[0].addEventListener('click', function () {
     toHide1.classList.toggle('d-none')
 })
-hideSectionButtons[1].addEventListener('click', function (){
+hideSectionButtons[1].addEventListener('click', function () {
     toHide2.classList.toggle('d-none')
 })
 const allTheCards = document.getElementsByClassName('card')
 console.log(allTheCards)
-const voyagesCounter = function (arr){
+const voyagesCounter = function (arr) {
     counter = 0
-    for(let i=0; i<arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         counter++
     }
     return counter
 }
 
 const navButton = document.getElementsByClassName('nav-button')
-navButton[0].addEventListener('click', function (){
-    alert('Attualmente sono disponibili: '+ voyagesCounter(allTheCards) + ' mete per il tuo viaggio')
+navButton[0].addEventListener('click', function () {
+    alert('Attualmente sono disponibili: ' + voyagesCounter(allTheCards) + ' mete per il tuo viaggio')
 })
 const allTheBadge = document.getElementsByClassName('badge')
-navButton[1].addEventListener('click', function (){
-     for(let i=0; i < allTheBadge; i++){
-         allTheBadge[i].classList.remove('invisible')
-         allTheBadge[i].classList.add('visible')
-         console.log(allTheBadge)
-     }
-    console.log('ciao')
+navButton[1].addEventListener('click', function () {
+    for (let i = 0; i < allTheBadge.length; i++) {
+        allTheBadge[i].classList.toggle('hidden')
+    }
 })
